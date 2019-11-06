67805977385df21e6b3c9c693277441e9524d6a5
abriku <oaoandrew@gmail.com>
2019-11-06 21:19:59 +0300

    feat: add key actions

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 import './style.css';
 import kdbkeys from './keys.js';
 
-
-
 const createEl = (tag, cls, addTo, tagvalue) => {
-    const el = document.createElement(tag);
-    if (cls != null) {
-      el.setAttribute('class', cls);
-    }
-    if (tagvalue != null) {
-      el.innerText = tagvalue;
-    }
-  
-    addTo.append(el);
-    return el;
-  };
+  const el = document.createElement(tag);
+  if (cls != null) {
+    el.setAttribute('class', cls);
+  }
+  if (tagvalue != null) {
+    el.innerText = tagvalue;
+  }
+
+  addTo.append(el);
+  return el;
+};
 
 const body = document.querySelector('body');
 const root = createEl('div', 'root', body, null);
@@ -24,13 +22,46 @@ const kbdContainer = createEl('div', 'keyboard', wrapper, null);
 
 
 kdbkeys.forEach((row) => {
-    const sectionRow = createEl('section', 'key-row', kbdContainer, null);
-    row.forEach((keyItem) => {
-        const keyDiv = createEl('div', `${keyItem[0]}`, sectionRow, keyItem[2]);
-    });
+  const sectionRow = createEl('section', 'key-row', kbdContainer, null);
+  row.forEach((keyItem) => {
+    const keyDiv = createEl('div', keyItem[0], sectionRow, keyItem[2]);
+    keyDiv.setAttribute('data-action', keyItem[1]);
+  });
 });
 
-const keyDownListener = (event) => {};
+const keyDownListener = (event) => {
+  event.preventDefault();
+  let someKey;
+  let setValue;
+  if (event.code) {
+    someKey = document.querySelector(`div[data-action=${event.code}]`);
+    setValue = event.key;
+  }
+  
+  if (event.target.hasAttribute('data-action')) {
+    someKey = event.target;
+    setValue = someKey.innerText;
+  }
 
-const KeyUpListener = (event) => {};
+  if (someKey) {
+    textArea.value += setValue;
+    someKey.classList.toggle('active');
+  }
+};
 
+const keyUpListener = (event) => {
+  event.preventDefault();
+  let someKey;
+  if (event.code === undefined) {
+    someKey = event.target;
+  }
+  else {
+    someKey = document.querySelector(`div[data-action=${event.code}]`);
+  }
+  someKey.classList.toggle('active');
+};
+
+document.addEventListener('keydown', keyDownListener);
+document.addEventListener('keyup', keyUpListener);
+document.addEventListener('mousedown', keyDownListener);
+document.addEventListener('mouseup', keyUpListener);
