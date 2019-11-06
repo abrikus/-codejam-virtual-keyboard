201cc92dac398065ee1201d938fccc95cb0a4ccd
abriku <oaoandrew@gmail.com>
2019-11-06 14:01:24 +0300

    feat: add basic project config files

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  mode: 'development',
+  module: {
+      rules: [
+          {
+            test: /\.html$/,
+            use: {
+              loader: 'html-loader',
+              options: { minimize: false },
+            }
+          },
+          {
+              test: /\.css$/,
+              use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              //'style-loader',
+              //Creates miniCSS
+              MiniCssExtractPlugin.loader,
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          },
+          {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,
+              use: {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'assets/fonts',
+              }
+            },
+          },
+          {
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  outputPath: 'assets/img',
+                }
+              },
+            ],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+      ]
+  },
+  plugins: [
+      new HtmlWebPackPlugin({
+          template: './index.html',
+          filename: './index.html'
+      }),
+  //],
+  //[
+      new MiniCssExtractPlugin({
+          filename: './style.css',
+          //chunkFilename: '[id].css',
+      })
+  ],
+  devtool: 'inline-source-map',
+
+
+  devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3000
+  }
+};
