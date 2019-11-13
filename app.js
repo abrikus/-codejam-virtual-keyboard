6c0da36c0ff594ce7fbb966b47119c548ab85de7
abriku <oaoandrew@gmail.com>
2019-11-13 22:50:26 +0300

    fix: bug fix and add switch lang and upperCase

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import './style.css';
-import kdbkeys from './keys.js';
+import kbdkeys from './keys';
+
+const states = {
+  firedKey: null,
+  ControlLeft: false,
+  AltLeft: false,
+  ShiftLeft: false,
+  lang: 'ru',
+};
 
 const createEl = (tag, cls, addTo, tagvalue) => {
   const el = document.createElement(tag);
@@ -14,6 +22,100 @@ const createEl = (tag, cls, addTo, tagvalue) => {
   return el;
 };
 
+const changeLang = (kbdKeysInst) => {
+  const { lang, ShiftLeft } = states;
+  if (lang === 'en' && ShiftLeft === false) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        global.console.log(key[1]);
+        const keyRu = key[2];
+        keyCurrent.innerText = keyRu;
+      });
+    });
+    states.lang = 'ru';
+  }
+
+  if (lang === 'en' && ShiftLeft === true) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        global.console.log(key[1]);
+        const keyRu = key[3];
+        keyCurrent.innerText = keyRu;
+      });
+    });
+    states.lang = 'ru';
+  }
+
+  if (lang === 'ru' && ShiftLeft === false) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        global.console.log(key[1]);
+        const keyEn = key[4];
+        keyCurrent.innerText = keyEn;
+      });
+    });
+    states.lang = 'en';
+  }
+
+  if (lang === 'ru' && ShiftLeft === true) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        global.console.log(key[1]);
+        const keyEn = key[5];
+        keyCurrent.innerText = keyEn;
+      });
+    });
+    states.lang = 'en';
+  }
+};
+
+const shiftUpper = (kbdKeysInst) => {
+  const { lang, ShiftLeft } = states;
+  if (lang === 'ru' && ShiftLeft === true) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        const keyRu = key[3];
+        keyCurrent.innerText = keyRu;
+      });
+    });
+  }
+
+  if (lang === 'ru' && ShiftLeft === false) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        const keyRu = key[2];
+        keyCurrent.innerText = keyRu;
+      });
+    });
+  }
+
+  if (lang === 'en' && ShiftLeft === true) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        const keyEn = key[5];
+        keyCurrent.innerText = keyEn;
+      });
+    });
+  }
+
+  if (lang === 'en' && ShiftLeft === false) {
+    kbdKeysInst.forEach((keyGroup) => {
+      keyGroup.forEach((key) => {
+        const keyCurrent = document.querySelector(`div[data-action=${key[1]}]`);
+        const keyEn = key[4];
+        keyCurrent.innerText = keyEn;
+      });
+    });
+  }
+};
+
 const body = document.querySelector('body');
 const root = createEl('div', 'root', body, null);
 const wrapper = createEl('div', 'wrapper', root, null);
@@ -21,7 +123,7 @@ const textArea = createEl('textarea', null, wrapper, null);
 const kbdContainer = createEl('div', 'keyboard', wrapper, null);
 
 
-kdbkeys.forEach((row) => {
+kbdkeys.forEach((row) => {
   const sectionRow = createEl('section', 'key-row', kbdContainer, null);
   row.forEach((keyItem) => {
     const keyDiv = createEl('div', keyItem[0], sectionRow, keyItem[2]);
@@ -39,44 +141,95 @@ const keyDownListener = (event) => {
   }
 
   if (event.target.hasAttribute('data-action')) {
+    global.console.log(event.target);
     someKey = event.target;
     setValue = someKey.innerText;
   }
 
-  if (someKey.getAttribute('data-action') === 'ShiftLeft'
-    || someKey.getAttribute('data-action') === 'ShiftRight'
+  if (someKey) {
+    if (states.firedKey !== null) {
+      states.firedKey.classList.remove('active');
+      states.firedKey = someKey;
+    } else {
+      states.firedKey = someKey;
+    }
+    if (someKey.getAttribute('data-action') === 'ShiftLeft'
     || someKey.getAttribute('data-action') === 'ControlLeft'
-    || someKey.getAttribute('data-action') === 'ControlRight'
     || someKey.getAttribute('data-action') === 'AltLeft') {
-    textArea.value += '';
-    someKey.classList.toggle('active');
-  } else if (someKey.getAttribute('data-action') === 'Tab') {
-    textArea.value += ' '.repeat(4);
-    someKey.classList.toggle('active');
-  } else if (someKey.getAttribute('data-action') === 'Enter') {
-    textArea.value += '\n';
-    someKey.classList.toggle('active');
-  } else if (someKey.getAttribute('data-action') === 'Space') {
-    textArea.value += ' ';
-    someKey.classList.toggle('active');
-  } else {
-    textArea.value += setValue;
-    someKey.classList.toggle('active');
+      const elAdv = someKey;
+      const { ShiftLeft, ControlLeft, AltLeft } = states;
+      if (elAdv.getAttribute('data-action') === 'ShiftLeft') {
+        if (ShiftLeft === true) {
+          states.ShiftLeft = false;
+          elAdv.classList.toggle('active');
+        }
+        if (ShiftLeft === false) {
+          states.ShiftLeft = true;
+          elAdv.classList.toggle('active');
+        }
+        shiftUpper(kbdkeys);
+      }
+
+      if (elAdv.getAttribute('data-action') === 'ControlLeft') {
+        if (ControlLeft === true) {
+          states.ControlLeft = false;
+          elAdv.classList.toggle('active');
+        }
+
+        if (ControlLeft === false) {
+          states.ControlLeft = true;
+          elAdv.classList.toggle('active');
+          if (AltLeft === true) {
+            changeLang(kbdkeys);
+            states.ControlLeft = false;
+            states.AltLeft = false;
+          }
+        }
+      }
+
+      if (elAdv.getAttribute('data-action') === 'AltLeft') {
+        if (AltLeft === true) {
+          states.AltLeft = false;
+          elAdv.classList.toggle('active');
+        }
+
+        if (AltLeft === false) {
+          states.AltLeft = true;
+          elAdv.classList.toggle('active');
+          if (ControlLeft === true) {
+            changeLang(kbdkeys);
+            states.ControlLeft = false;
+            states.AltLeft = false;
+          }
+        }
+      }
+
+      global.console.log(states);
+    } else if (someKey.getAttribute('data-action') === 'Tab') {
+      textArea.value += ' '.repeat(4);
+      someKey.classList.toggle('active');
+    } else if (someKey.getAttribute('data-action') === 'Enter') {
+      textArea.value += '\n';
+      someKey.classList.toggle('active');
+    } else if (someKey.getAttribute('data-action') === 'Space') {
+      textArea.value += ' ';
+      someKey.classList.toggle('active');
+    } else if (someKey.classList.contains('key')) {
+      textArea.value += setValue;
+      someKey.classList.toggle('active');
+    }
   }
 };
 
-const keyUpListener = (event) => {
-  event.preventDefault();
-  let someKey;
-  if (event.code === undefined) {
-    someKey = event.target;
-  } else {
-    someKey = document.querySelector(`div[data-action=${event.code}]`);
+const keyUpListener = () => {
+  let { firedKey } = states;
+  if (firedKey !== null) {
+    firedKey.classList.toggle('active');
+    firedKey = null;
   }
-  someKey.classList.toggle('active');
 };
 
-document.addEventListener('keydown', keyDownListener);
-document.addEventListener('keyup', keyUpListener);
-document.addEventListener('mousedown', keyDownListener);
-document.addEventListener('mouseup', keyUpListener);
+window.addEventListener('keydown', keyDownListener);
+window.addEventListener('keyup', keyUpListener);
+kbdContainer.addEventListener('mousedown', keyDownListener);
+kbdContainer.addEventListener('mouseup', keyUpListener);
--- webpack.config.js
+++ webpack.config.js
@@ -6,85 +6,77 @@ module.exports = {
   entry: './app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.js',
   },
   mode: 'development',
   module: {
-      rules: [
-          {
-            test: /\.html$/,
-            use: {
-              loader: 'html-loader',
-              options: { minimize: false },
-            }
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: { minimize: false },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets/fonts',
           },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
           {
-              test: /\.css$/,
-              use: [MiniCssExtractPlugin.loader, 'css-loader'],
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              //'style-loader',
-              //Creates miniCSS
-              MiniCssExtractPlugin.loader,
-              // Translates CSS into CommonJS
-              'css-loader',
-              // Compiles Sass to CSS
-              'sass-loader',
-            ],
-          },
-          {
-              test: /\.(woff|woff2|eot|ttf|otf)$/,
-              use: {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'assets/fonts',
-              }
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/img',
             },
           },
-          {
-            test: /\.(png|jpe?g|gif|svg)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  outputPath: 'assets/img',
-                }
-              },
-            ],
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          },
-      ]
+        },
+      },
+    ],
   },
   plugins: [
-      new HtmlWebPackPlugin({
-          template: './index.html',
-          filename: './index.html'
-      }),
-  //],
-  //[
-      new MiniCssExtractPlugin({
-          filename: './style.css',
-          //chunkFilename: '[id].css',
-      })
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './style.css',
+    }),
   ],
   devtool: 'inline-source-map',
 
 
   devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      port: 3000
-  }
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
 };
